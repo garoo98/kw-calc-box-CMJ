cd93b0b6d94a531d88d44dcbe7d4f7d54a46f86d
CMJ <garoo98@naver.com>
2023-11-02 07:08:50 +0900

    second commit

--- lib.js
+++ lib.js
@@ -4,11 +4,21 @@ function avg(numbers) {
 }
 
 function prime(num) {
-    return false;
+    if (num <= 1)   return false;
+    else if (num <= 3) return true;
+    if(num % 2 === 0 || num % 3 === 0) return false;
+    for(let i = 5; i*i<=num; i+=6)
+    {
+        if(num % i === 0 || num % (i+2) === 0) return false;
+    }
+
+    return true;
 }
 
-function fact(num) {
-    return -1;
+function fact(num) { 
+    if(num === 1) return 1;
+    
+    return num*fact(num-1);
 }
 
 module.exports = {
--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,23 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("Prime 29 should be Ture", () => {
+    expect(lib.prime(29)).toBe(true);
+});
+
+test("Prime 17 should be Ture", () => {
+    expect(lib.prime(29)).toBe(true);
+});
+
+test("Prime 15 should be False", () => {
+    expect(lib.prime(15)).toBe(false);
+});
+
+test("Factorial 6 should be 720", () => {
+    expect(lib.fact(6)).toBe(720);
+});
+
+test("Factorial 10 should be 3628800", () => {
+    expect(lib.fact(10)).toBe(3628800);
+});
